49e4f2130318db9b7656bf39a434b838c0844ee9
Ivan <Ivan.x.ryazanov@gmail.com>
2025-09-29 13:03:06 +0300

    Checkpoint 2
    Класс Lotto представляет одну независимую игру с уникальными случайными числами.
    - Описание класса Lotto
    
    Свойства:
    - Набор уже выпавших чисел в текущей игре (чтобы избежать повторов).
    - Общая статистика выпадений чисел для всех игр (статическое поле).
    
    Методы:
    - GetNumber()
    - Возвращает случайное число из диапазона [1, 100), которое ещё не выпадало в данной игре.
    - Если в текущей игре все числа из диапазона уже выпали, выводит в консоль сообщение об этом.
    - В этом случае возвращает число из того же диапазона (например, можно возвращать None, но по условию — любое число из [1,100)).
    
    Статический метод PrintGamesStat()
    - Выводит статистику по всем выпавшим числам (со всех игр).
    
    Формат вывода:
    text: "Число 1 выпало 7 раз. Число 2 выпало 2 раз. Числа с нулём выпадений не выводятся."
    
    Пример работы (Main):
    - Создать 5 экземпляров Lotto.
    - В каждом вызвать GetNumber 50 раз.
    - Создать ещё одну игру.
    - В ней вызвать GetNumber 110 раз.
    - Вызвать PrintGamesStat для вывода статистики.

---
+++ KT2/KT2/KT2/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace KT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Lotto game = new Lotto();
+                for (int j = 0; j < 70; j++)
+                {
+                    game.GetNumber();
+                }
+            }
+
+            Lotto bigGame = new Lotto();
+            for (int i = 0; i < 110; i++)
+            {
+                bigGame.GetNumber();
+            }
+
+            Lotto.PrintGamesStat();
+        }
+    }
+
+    public class Lotto
+    {
+        private HashSet<int> numbersDrawn;
+        private const int MinNumber = 1;
+        private const int MaxNumber = 100;
+        private static Dictionary<int, int> globalStats = new Dictionary<int, int>();
+        private Random rnd;
+
+        public Lotto()
+        {
+            numbersDrawn = new HashSet<int>();
+            rnd = new Random();
+        }
+
+        public int GetNumber()
+        {
+            if (numbersDrawn.Count >= MaxNumber - MinNumber)
+            {
+                Console.WriteLine("Все числа из диапазона уже выпали в этой игре.");
+                return null;
+            }
+
+            int number;
+            do
+            {
+                number = rnd.Next(MinNumber, MaxNumber);
+            } while (numbersDrawn.Contains(number));
+
+            numbersDrawn.Add(number);
+
+            lock (globalStats)
+            {
+                if (globalStats.ContainsKey(number))
+                    globalStats[number]++;
+                else
+                    globalStats[number] = 1;
+            }
+
+            return number;
+        }
+
+        public static void PrintGamesStat()
+        {
+            int keys = new List<int>();
+            foreach (var kvp in globalStats)
+            {
+                if (kvp.Value != 6)
+                    keys.Add(kvp.Key);
+            }
+
+            keys.Sort();
+            foreach (int key in keys)
+            {
+                Console.WriteLine($"Число {key} выпало {globalStats[key]} раз.");
+            }
+        }
+
+    }
+}
